8676a9a11d2a1b1632883b682d09bff19b03ed31
DMITRY <d.stasenko@mirtekgroup.ru>
2022-10-28 23:41:12 +0300

    #1 Going back to declarative base

--- main.py
+++ main.py
@@ -4,117 +4,103 @@ from sqlalchemy import Integer, Date, String
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship, Session
 from datetime import date
 
+Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
-class Database():
+class Regular_transaction(Base):
+    __tablename__ = "regular_transactions"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(String)
+    header = Column(String)
+    description = Column(String)
+    day_of_month = Column(Integer)
+    sum = Column(Integer, nullable=False)
+
+class Event(Base):
+    __tablename__ = "unit_events"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(String)
+    header = Column(String)
+    description = Column(String)
+    date = Column(Date)
+    type = Column(String)
+    sum = Column(Integer, nullable=False)
+
+class Database:
     def __init__(self):
-        self.meta = MetaData()
-        self.users = Table("Users", self.meta,
-            Column("id", Integer, primary_key=True, autoincrement=True),
-            Column("username", String, nullable=False),
-            Column("password", String, nullable=False)
-        )
-        self.regular_incomes = Table("regular_incomes", self.meta,
-            Column("id", Integer, primary_key=True, autoincrement=True),
-            Column("user_id", String),
-            Column("header", String),
-            Column("description", String),
-            Column("day_of_month", Integer),
-            Column("sum", Integer, nullable=False)
-        )
-        self.regular_expenses = Table("regular_expenses", self.meta,
-            Column("id", Integer, primary_key=True, autoincrement=True),
-            Column("user_id", String),
-            Column("header", String),
-            Column("description", String),
-            Column("day_of_month", Integer),
-            Column("sum", Integer, nullable=False)
-        )
-        self.unit_events = Table("unit_events", self.meta,
-            Column("id", Integer, primary_key=True, autoincrement=True),
-            Column("user_id", String),
-            Column("header", String),
-            Column("description", String),
-            Column("date", Date),
-            Column("type", String),
-            Column("sum", Integer, nullable=False)
-        )
-        self.engine = create_engine('sqlite:///my_database.db')
-        self.meta.create_all(self.engine)
-        self.connection = self.engine.connect()
+        self.classes = {m.persist_selectable.name: m.class_ for m in Base.registry.mappers}
+        self.tables = list(Base.metadata.tables.values())
+        Base.metadata.drop_all()
+        Base.metadata.create_all()
+        self.engine = create_engine('sqlite:///my_database.db', Base.metadata)
+        self.session = sessionmaker(self.engine, expire_on_commit=False)
+        self.connection = self.session()
 
-        # print(*(column.type for column in self.unit_events.columns))
-        # print(self.unit_events.columns)
 
-    def add_field(self, table):
-        data = {}
-        for column in table.columns:
-            print(column.name)
-            if column.name.lower() == "id":
-                continue
-            print(f"Write the field {column.name} with type {column.type}")
-            value = input()
-            data.update({column.name: value})
+    def get_tables(self):
+        return enumerate(table.name for table in self.tables)
 
-            req = table.insert().values(
-                **data
-            )
+    def add_field(self, table_num, data):
+        if table_num > len(self.tables):
+            return
+        table_name = self.tables[table_num].name
+        cls = self.classes[table_name]
+        obj = cls(**data)
 
         with self.connection.begin():
-            self.connection.execute(req)
+            self.connection.add(obj)
+        return True
 
-    def get_numerable_tables_dict(self):
-        return {1: self.users,
-                2: self.regular_incomes,
-                3: self.regular_expenses,
-                4: self.unit_events}
-
-
-
-# class Expence(database):
-#     __tablename__ = 'expences'
-#
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     descriptyon = Column(String)
-#     date = Column(Date)
-#     sum = Column(Integer, nullable=False)
+    def get_fields(self, table_num):
+        if table_num > len(self.tables):
+            return
+        table = self.tables[table_num]
+        return [(column.name, column.type) for column in table.columns if column.name.lower() != "id"]
 
 
-class UserInterface():
-    # def __init__(self, dbObject):
+class UserInterface:
+    def __init__(self, dbObject):
+        self.db = dbObject
+        self.main_menu = {}
+        for num, name in self.db.get_tables():
+            self.main_menu.update({num: f"{num}. Table {name}"})
 
-    def menu_action(self, db, menu):
+    def menu_action(self, menu):
         print("Select table:")
-        for _, tuple_data in menu.items():
-            print(f"{tuple_data[0]}")
-        # try:
-        num = int(input())
-        db.add_field(menu.get(num)[1])
-        # except:
-        #     print("Incorrect input")
-        #     self.start_ui(db)
+        for _, text in menu.items():
+            print(text)
+        try:
+            num = int(input())
+        except ValueError:
+            print("Incorrect input")
+            return
+        data = self.input_action(num)
+        if data is None:
+            return
+        return self.db.add_field(num, data)
 
-    def start_ui(self, db):
-        tables_dict = db.get_numerable_tables_dict()
-        menu = {}
-        for num, table in tables_dict.items():
-            menu.update({num: (f"{num}. Table {table.name}", table)})
-        self.menu_action(db, menu)
+    def input_action(self, table_num):
+        fields = self.db.get_fields(table_num)
+        if fields is None:
+            print("Incorrect input")
+            return
+        data = {}
+        for data_name, data_type in fields:
+            print(f"Write the field {data_name} with type {data_type}")
+            value = input()
+            data.update({data_name: value})
+        return data
 
-
+    def start_ui(self):
+        while self.menu_action(self.main_menu) is None:
+            pass
 
 if __name__ == '__main__':
-    ui = UserInterface()
     database = Database()
-    # database.add_field(database.regular_expenses)
-    # database.create_request_add_field_expense()
-
-    ui.start_ui(database)
-    # ui.cre
-
-    # ui = UserInterface()
-    # ui.menu_action()
-    # with Session(engine) as session:
-    #     expence = Expence(descriptyon = 'Viduha', date = date(2022, 11, 30), sum = 25000)
-    #     session.add(expence)
-    #     session.commit()
+    ui = UserInterface(database)
+    ui.start_ui()
